bcc6b90877797843bd07e471bd9fdb63c9034fba
shisanlang <shisanlangd@gmail.com>
2013-08-28 16:40:20 +0800

    音乐属性

--- ipod/ipod/ViewController.m
+++ ipod/ipod/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
 }
 
 - (void)didReceiveMemoryWarning
