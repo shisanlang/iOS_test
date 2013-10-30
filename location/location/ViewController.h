bce6fd1bf8d78e6e0c71798d88b7f6de1f1fef70
shisanlang <shisanlangd@gmail.com>
2013-10-30 14:23:20 +0800

    add

--- location/location/ViewController.h
+++ location/location/ViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<CLLocationManagerDelegate>
+
+@property(nonatomic,retain) UILabel * PlaceLabel;
+@property(nonatomic,retain) UILabel * PlaceLabel2;
+@property(nonatomic,retain) CLLocationManager *locationManager;
 
 @end
--- location/location/ViewController.m
+++ location/location/ViewController.m
@@ -8,16 +8,59 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
+@synthesize PlaceLabel,PlaceLabel2;
+@synthesize locationManager;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    PlaceLabel = [[UILabel alloc]initWithFrame:CGRectMake(40, 130, 240, 15)];
+    PlaceLabel.text = @"";//[self localIPAddress];
+    PlaceLabel.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:PlaceLabel];
+    
+    PlaceLabel2 = [[UILabel alloc]initWithFrame:CGRectMake(40, 180, 240, 15)];
+    PlaceLabel2.text = @"";//[self localIPAddress];
+    PlaceLabel2.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:PlaceLabel2];
+    
+    
+    locationManager = [[CLLocationManager alloc] init];//创建位置管理器
+    locationManager.delegate=self;
+    locationManager.desiredAccuracy=kCLLocationAccuracyBest;
+    locationManager.distanceFilter=0.01;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    //启动位置更新
+    [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(refresh) userInfo:nil repeats:YES];
+    
+//    [locationManager startUpdatingLocation];
+}
+
+- (void)refresh
+{
+    [locationManager startUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation
+{
+    PlaceLabel.text = [NSString stringWithFormat:@"%f------%f",oldLocation.coordinate.latitude,oldLocation.coordinate.longitude];
+    PlaceLabel2.text = [NSString stringWithFormat:@"%f------%f",newLocation.coordinate.latitude,newLocation.coordinate.longitude];
+}
+-(void)locationManager:(CLLocationManager *)manager
+      didFailWithError:(NSError *)error
+{
+    NSLog(@"error");
 }
 
 - (void)didReceiveMemoryWarning
