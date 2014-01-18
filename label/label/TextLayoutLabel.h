441ac0487d28470b0e8bbf7ac4c2353795092369
shisanlangd <shisanlangd@gmail.com>
2014-01-18 16:23:02 +0800

    两端对齐,行间距

---
+++ label/label/TextLayoutLabel.h
@@ -0,0 +1,13 @@
+//
+//  TextLayoutLabel.h
+//  label
+//
+//  Created by duan on 14-1-18.
+//  Copyright (c) 2014年 duan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextLayoutLabel : UILabel
+
+@end
---
+++ label/label/TextLayoutLabel.m
@@ -0,0 +1,31 @@
+//
+//  TextLayoutLabel.m
+//  label
+//
+//  Created by duan on 14-1-18.
+//  Copyright (c) 2014年 duan. All rights reserved.
+//
+
+#import "TextLayoutLabel.h"
+
+@implementation TextLayoutLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
