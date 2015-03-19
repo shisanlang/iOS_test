e27cfe6dbb5096adc2330507965f72ad6a058ff7
shisanlang <shisanlangd@gmail.com>
2015-03-19 18:48:27 +0800

    widget

---
+++ widget/iwidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  iwidget
+//
+//  Created by dxd on 15/3/19.
+//  Copyright (c) 2015年 dxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ widget/widget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  widget
+//
+//  Created by dxd on 15/3/19.
+//  Copyright (c) 2015年 dxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
