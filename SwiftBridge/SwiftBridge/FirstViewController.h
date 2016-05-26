5ffd661749714d6c8e0c0cc3394dee819ced795b
shisanlang <shisanlangd@gmail.com>
2016-05-26 17:44:50 +0800

    sw

---
+++ SwiftBridge/SwiftBridge/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  SwiftBridge
+//
+//  Created by 十三郎 on 16/5/19.
+//  Copyright © 2016年 QianHai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ SwiftBridge/SwiftBridge/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  SwiftBridge
+//
+//  Created by 十三郎 on 16/5/19.
+//  Copyright © 2016年 QianHai. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
