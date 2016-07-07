e77df115f1e7ffb1fc002998b6415d0906bb1f14
shisanlang <shisanlangd@gmail.com>
2016-07-07 19:49:24 +0800

    rm watch

---
+++ watch2/watch2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watch2
+//
+//  Created by 十三郎 on 16/7/7.
+//  Copyright © 2016年 QianHai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
