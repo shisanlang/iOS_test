01c265eaede9c4efc06628442a1fc723c3fd931e
shisanlang <shisanlangd@gmail.com>
2018-08-24 14:50:19 +0800

    language 设置,多语言 地区

---
+++ language/language/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  language
+//
+//  Created by 十三郎 on 2018/8/24.
+//  Copyright © 2018年 QianHai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ language/language/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  language
+//
+//  Created by 十三郎 on 2018/8/24.
+//  Copyright © 2018年 QianHai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
